1e9b69f3fc885c24d0b8ce85b01736163f8c9f87
axel <axel.rl.404@gmail.com>
2023-05-21 12:52:57 +0700

    init: home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Head from '$lib/components/Head.svelte';
+	import PortraitPage from '$lib/components/pages/PortraitPage.svelte';
+	import Cover from './Cover.svelte';
+</script>
+
+<Head title="Home" description="Manga data in manga style." />
+
+<div class="grid gap-5">
+	<PortraitPage>
+		<Cover />
+	</PortraitPage>
+</div>
---
+++ src/routes/Cover.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Border from '$lib/components/Border.svelte';
+	import Image from '$lib/components/Image.svelte';
+</script>
+
+<div class="p-5 h-full w-full">
+	<Image
+		src="/inazuma.png"
+		alt="inazuma"
+		class="w-full h-full object-cover object-center grayscale border-2 border-black bg-gradient-to-b from-white to-neutral-400"
+	/>
+
+	<div
+		class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 grid text-center shadow-white"
+	>
+		<div class="text-3xl font-bold text-shadow-sm">INAZUMA</div>
+		<Border />
+		<div class="text-shadow-sm">Manga data in manga style</div>
+	</div>
+</div>
